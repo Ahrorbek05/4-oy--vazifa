aab57afc0e3a63b12da5e5d1bbd80038204b6f60
Ahrorbek05 <akramjonovahrorbek@gmail.com>
2024-05-22 12:43:58 +0500

    uy vazifa

---
+++ js/main.js
@@ -0,0 +1,177 @@
+// 1-masala
+// let A = +prompt('A musbat son kiriting!!!');
+// if ( A > 0 ){
+//     console.log('rost');
+// } else {
+//     console.log('yolgon');
+// }
+
+// 2-masala
+// let A = +prompt('A toq son kiriting!!!');
+// if ( A % 2 == 1 ){
+//     console.log('rost');
+// } else {
+//     console.log('yolgon');
+// }
+
+// 3-masala
+// let A = +prompt('A juft son kiriting!!!');
+// if  ( A % 2 == 0 ){
+//     console.log('rost');
+// } else {
+//     console.log('yolgon');
+// }
+
+// 4-masala
+// let A = +prompt('A sonni kiriting');
+// let B = +prompt('B sonni kiriting');
+
+// if ( A > 2 %  B <= 3 ){
+//     console.log('rost');
+// } else {
+//     console.log('yolgon');
+// }
+
+
+// 5-masala
+// let A = +prompt('A sonni kiriting');
+// let B = +prompt('B sonni kiriting');
+// if ( A >= 0 || B < -2 ){
+//     console.log('rost');
+// } else {
+//     console.log('yolgon');
+// }
+
+// 6-masala
+// let A = +prompt('A sonni kiriting');
+// let B = +prompt('B sonni kiriting');
+// let C = +prompt('C sonni kiriting');
+// if ( A <= B ){
+//     console.log('true');
+// } else {
+//     console.log('false');
+// }
+
+// if ( B <= C ){
+//     console.log('true');
+// } else {
+//     console.log('false');
+// }
+
+// 8-masala
+// let A = +prompt('A sonni kiriting');
+// let B = +prompt('B sonni kiriting');
+// if ( A % 2 == 1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+// if ( B % 2 == 1 ){
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 10-masala
+// let A = +prompt("A son");
+// let B = +prompt("B son");
+// if (A % 2==1 % B % 2==1){
+//   console.log( true );
+// }
+// else{
+//   console.log(false );
+// }
+
+// 11-masala
+// let A = +prompt("A son");
+// let B = +prompt("B son");
+// if (A % 2==1 && B % 2==1  A % 2==0 && B % 2==0){
+//   console.log( true );
+// }
+// else{
+//   console.log(false );
+// }
+
+// 12-masala
+// let A = +prompt("A son");
+// let B = +prompt("B son");
+// let C = +prompt("C son");
+// if (A >= 0 && B >= 0) {
+//   console.log(false);
+// }
+// else if (A >= 0 && C >= 0) {
+//   console.log(false);
+// }
+// else if (B >= 0 && C >= 0) {
+//   console.log(false);
+// }
+// else {
+//   console.log(true);
+// }
+
+// 13-masala
+// let A = +prompt("A son");
+// let B = +prompt("B son");
+// let C = +prompt("C son");
+// if (A >= 0 && B >= 0) {
+//   console.log(true);
+// }
+// else if (A >= 0  C >= 0) {
+//   console.log(true);
+// }
+// else if (B >= 0  C >= 0) {
+//   console.log(true);
+// }
+// else {
+//   console.log(false);
+// }
+
+// 14-masala
+// let A = +prompt("A son");
+// let B = +prompt("B son");
+// let C = +prompt("C son");
+// if (A >= 0  B >= 0) {
+//   console.log(true);
+// }
+// else if (A >= 0  C >= 0) {
+//   console.log(true);
+// }
+// else if (B >= 0 || C >= 0) {
+//   console.log(true);
+// }
+// else {
+//   console.log(false);
+// }
+
+// 15-masala
+// let A = +prompt("A son");
+// let B = +prompt("B son");
+// let C = +prompt("C son");
+// if (A >= 0 && B >= 0) {
+//   console.log(true);
+// }
+// else if (A >= 0 && C >= 0) {
+//   console.log(true);
+// }
+// else if (B >= 0 && C >= 0) {
+//   console.log(true);
+// }
+// else {
+//   console.log(false);
+// }
+
+// 16-masala
+// let A = +prompt("A son");
+// if (A > 9 && A < 100 && A % 2 == 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 17-masala
+// let A = +prompt("A son");
+// if (A > 100 && A < 1000 &&  A % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
